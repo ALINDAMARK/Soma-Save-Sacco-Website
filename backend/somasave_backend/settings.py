13e1ef61ade4c7cea1b4121bdf3453d275d30421
kalosius <kalosius43@gmail.com>
2025-12-18 13:25:32 +0300

    Pus in Env

--- backend/somasave_backend/settings.py
+++ backend/somasave_backend/settings.py
@@ -260,6 +260,7 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
+
 cloudinary.config(
     cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME', 'dhgjydahn'),
     api_key=os.getenv('CLOUDINARY_API_KEY', '617993754119547'),
